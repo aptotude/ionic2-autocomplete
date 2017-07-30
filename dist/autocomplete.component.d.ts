95fc4da31b866b945acb6e1250428d11ba29c5a1
Jason Knisley <jasonk@apto.com>
2017-07-30 11:41:17 -0600

    Updated README. Added missing dist files.

---
+++ dist/autocomplete.component.d.ts
@@ -0,0 +1,74 @@
+import { EventEmitter, TemplateRef } from '@angular/core';
+export declare class AutoCompleteComponent {
+    dataProvider: any;
+    options: any;
+    keyword: string;
+    showResultsFirst: boolean;
+    alwaysShowList: boolean;
+    hideListOnSelection: boolean;
+    template: TemplateRef<any>;
+    useIonInput: boolean;
+    itemSelected: EventEmitter<any>;
+    itemsShown: EventEmitter<any>;
+    itemsHidden: EventEmitter<any>;
+    ionAutoInput: EventEmitter<string>;
+    searchbarElem: any;
+    inputElem: any;
+    showList: boolean;
+    suggestions: string[];
+    private defaultOpts;
+    private selection;
+    private _showList;
+    /**
+     * create a new instace
+     */
+    constructor();
+    /**
+     * get items for auto-complete
+     */
+    getItems(): void;
+    /**
+     * show item list
+     */
+    showItemList(): void;
+    /**
+     * hide item list
+     */
+    hideItemList(): void;
+    /**
+     * select item from list
+     *
+     * @param event
+     * @param selection
+     **/
+    select(selection: any): void;
+    /**
+     * get current selection
+     * @returns {any}
+     */
+    getSelection(): any;
+    /**
+     * get current input value
+     * @returns {string}
+     */
+    getValue(): string;
+    /**
+     * set current input value
+     */
+    setValue(value: string): void;
+    /**
+  
+     /**
+     * clear current input value
+     */
+    clearValue(hideItemList?: boolean): void;
+    /**
+     * set focus of searchbar
+     */
+    setFocus(): void;
+    /**
+     * handle document click
+     * @param event
+     */
+    private documentClickHandler(event);
+}
